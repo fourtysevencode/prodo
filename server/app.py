bd95fa0e66b9c489727fee832895ec034a7f17a5
Ronak <ronakpalforyou@gmail.com>
2026-07-18 01:57:11 +0530

    added one origin to CORS Middleware

--- server/app.py
+++ server/app.py
@@ -29,6 +29,7 @@ app.add_middleware(
         "https://tauri.localhost",
         "https://prodo-live.pages.dev",
         "https://website-dev.prodo-live.pages.dev",
+        "https:/prodo.live"
     ],
     allow_credentials=True,
     allow_methods=["*"],
