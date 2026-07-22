74f7a66c092cc17e99ac753a68df30900aaabd40
Ivan Samuel <kazenoko.main@gmail.com>
2026-07-22 18:59:47 +0530

    Remove unused files

--- server/worker.py
+++
@@ -1,7 +0,0 @@
-from workers import WorkerEntrypoint
-from app import app
-
-class Default(WorkerEntrypoint):
-    async def fetch(self, request):
-        import asgi
-        return await asgi.fetch(app, request.js_object, self.env)
